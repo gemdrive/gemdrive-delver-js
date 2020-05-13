15aab3184f831a34d50794884b4fc79676efa3ce
Anders Pitman <tapitman11@gmail.com>
2020-05-12 19:38:41 -0600

    Remove a bunch of old commented code

--- index.js
+++ index.js
@@ -162,98 +162,6 @@ const RemFSDelver = async (options) => {
   return dom;
 
 
-
-
-  //const urlParams = new URLSearchParams(window.location.search);
-
-  //const previewDirName = 'previews';
-
-  //let curDir;
-  //let curPath;
-  //let remfsRoot;
-  //let layout = 'list';
-  //// TODO: don't really like having this be a global
-  //let onAddChild = null;
-  //let onRemoveChild = null;
-  //let selectedItems = {};
-
-  //if (urlParams.has('error')) {
-  //  alert("Authorization error. You probably don't have permission to view this path");
-  //  return;
-  //}
-
-  //let rootUri;
-  //if (urlParams.has('remfs')) {
-  //  rootUri = urlParams.get('remfs');
-  //}
-  //else if (options && options.rootUri) {
-  //  rootUri = options.rootUri;
-  //}
-
-  //if (!rootUri) {
-  //  dom.innerText = "Error: No remFS URI provided (remfs=<remfs-uri> parameter)";
-  //  return dom;
-  //}
-
-  //let rootUrl = rootUri;
-  //if (!rootUrl.startsWith('http')) {
-  //  const proto = options && options.secure ? 'https://' : 'http://';
-  //  rootUrl = proto + rootUrl;
-  //}
-
-  //history.pushState(null, '', window.location.pathname + '?remfs=' + rootUri);
-
-  //const controlBar = ControlBar();
-  //dom.appendChild(controlBar.dom);
-
-
-  //// File uploads
-  //const uppie = new Uppie();
-
-  //const handleFiles = async (e, formData, filenames) => {
-  //  for (const param of formData.entries()) {
-  //    const file = param[1];
-
-  //    console.log(file);
-
-  //    const uploadPath = [...curPath, file.name];
-  //    console.log(uploadPath);
-
-  //    fetch(rootUrl + encodePath(uploadPath) + '?access_token=' + localStorage.getItem('access_token'), {
-  //      method: 'PUT',
-  //      body: file,
-  //    })
-  //    .then(response => response.json())
-  //    .then(remfs => {
-
-  //      const currentItem = curDir.children[file.name];
-  //      curDir.children[file.name] = remfs;
-
-  //      if (!currentItem) {
-  //        // New item; update dom
-  //        onAddChild(file.name, remfs);
-  //      }
-  //    })
-  //    .catch(e => {
-  //      console.error(e);
-  //    });
-  //  }
-  //};
-
-  //const fileInput = InvisibleFileInput();
-  //uppie(fileInput, handleFiles);
-  //dom.appendChild(fileInput);
-  //
-  //const folderInput = InvisibleFolderInput();
-  //uppie(folderInput, handleFiles);
-  //dom.appendChild(folderInput);
-
-  //controlBar.dom.addEventListener('upload', (e) => {
-  //  fileInput.click();
-  //});
-
-  //const rootPath = new URL(rootUrl).pathname;
-
   //controlBar.dom.addEventListener('delete', (e) => {
   //  const numItems = Object.keys(selectedItems).length;
 
@@ -281,62 +189,6 @@ const RemFSDelver = async (options) => {
   //  }
   //});
 
-  //
-  //render();
-
-  //async function render() {
-
-  //  if (urlParams.has('code')) {
-  //    const code = urlParams.get('code');
-  //    urlParams.delete('code');
-
-  //    const accessToken = await fetch(rootUrl + '?pauth-method=token&grant_type=authorization_code&code=' + code).then(r => r.text());
-  //    localStorage.setItem('access_token', accessToken);
-  //  }
-
-  //  const remfsResponse = await fetch(rootUrl + '/remfs.json?access_token=' + localStorage.getItem('access_token'))
-
-  //  if (remfsResponse.status === 200) {
-  //    //await maintainInsecureToken(rootUrl, localStorage.getItem('access_token'));
-
-  //    remfsRoot = await remfsResponse.json();
-  //    curDir = remfsRoot;
-  //    curPath = [];
-
-  //    const dirContainer = document.createElement('div');
-  //    dirContainer.classList.add('remfs-delver__dir-container');
-  //    dom.appendChild(dirContainer);
-
-  //    const dir = Directory(remfsRoot, curDir, rootUrl, curPath, layout, localStorage.getItem('access_token'));
-  //    onAddChild = dir.onAddChild;
-  //    onRemoveChild = dir.onRemoveChild;
-  //    dirContainer.appendChild(dir.dom);
-
-  //    dirContainer.addEventListener('change-dir', (e) => {
-  //      selectedItems = {};
-  //      controlBar.onSelectedItemsChange(selectedItems);
-  //      curDir = remfsRoot;
-  //      curPath = e.detail.path;
-  //      controlBar.onPathChange(curPath);
-  //      for (const part of curPath) {
-  //        curDir = curDir.children[part];
-  //      }
-
-  //      history.pushState(null, '', window.location.pathname + '?remfs=' + rootUri + encodePath(curPath));
-
-  //      if (curDir.children) {
-  //        updateDirEl();
-  //      }
-  //      else {
-  //        fetch(rootUrl + encodePath(curPath) + '/remfs.json')
-  //        .then(response => response.json())
-  //        .then(remfs => {
-  //          curDir.children = remfs.children;
-  //          updateDirEl();
-  //        });
-  //      }
-  //    });
-
   //    dirContainer.addEventListener('item-selected', (e) => {
   //      selectedItems[encodePath(e.detail.path)] = true;
   //      controlBar.onSelectedItemsChange(selectedItems);
@@ -345,23 +197,6 @@ const RemFSDelver = async (options) => {
   //      delete selectedItems[encodePath(e.detail.path)];
   //      controlBar.onSelectedItemsChange(selectedItems);
   //    });
-
-  //    function updateDirEl() {
-  //      const newDir = Directory(remfsRoot, curDir, rootUrl, curPath, layout, localStorage.getItem('access_token'))
-  //      onAddChild = newDir.onAddChild;
-  //      onRemoveChild = newDir.onRemoveChild;
-  //      dirContainer.replaceChild(newDir.dom, dirContainer.childNodes[0]);
-  //    }
-  //  }
-  //  else if (remfsResponse.status === 403) {
-  //    const clientId = window.location.origin;
-  //    const redirectUri = window.location.href;
-  //    const scope = rootPath + ':write';
-  //    window.location.href = rootUrl + `?pauth-method=authorize&response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
-  //  }
-  //}
-
-  //return dom;
 };
 
 
@@ -431,54 +266,6 @@ function authorize(fsUrl) {
   const scope = '/:write';
   window.location.href = fsUrl + `?pauth-method=authorize&response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
 }
-
-
-// uses a long-lived token to regularly refresh a global short-lived, read-only
-// token. This is useful for things like setting image src URLs for protected
-// images.
-//async function maintainInsecureToken(rootUrl, secureToken) {
-//
-//  async function refreshToken() {
-//    return fetch(rootUrl + '?pauth-method=dummy&access_token=' + secureToken, {
-//      method: 'POST',
-//      headers: {
-//        'Content-Type': 'application/json',
-//      },
-//      body: JSON.stringify({
-//        method: 'authorize',
-//        params: {
-//          maxAge: 600,
-//          perms: {
-//            '/': {
-//              read: true,
-//            }
-//          }
-//        },
-//      }),
-//    })
-//    .then(response => {
-//      if (response.status !== 200) {
-//        alert("failed to refresh token");
-//      }
-//
-//      return response.text();
-//    })
-//    .then(token => {
-//      // Create a temporary link which includes a token, click that link, then
-//      // remove it.
-//      window.insecureToken = token;
-//
-//      return token;
-//    });
-//  }
-//
-//  await refreshToken();
-//
-//  // refresh every 9 minutes. Valid for 10 minutes
-//  setInterval(async () => {
-//    await refreshToken();  
-//  }, 540000);
-//}
 
 
 export {
