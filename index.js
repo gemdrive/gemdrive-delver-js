6d6ad27144e5af8fdbbe54fda65b449763661117
Anders Pitman <tapitman11@gmail.com>
2020-05-23 17:04:56 -0600

    Implement OAuth2 PKCE

--- index.js
+++ index.js
@@ -42,7 +42,10 @@ const RemFSDelver = async (options) => {
     //history.pushState(null, '', window.location.pathname + '?' + decodeURIComponent(urlParams.toString()));
     history.pushState(null, '', window.location.pathname);
 
-    const accessToken = await fetch(fsUrl + '?pauth-method=token&grant_type=authorization_code&code=' + code)
+    const codeVerifier = localStorage.getItem('pkceCodeVerifier');
+    localStorage.removeItem('pkceCodeVerifier');
+
+    const accessToken = await fetch(fsUrl + `?pauth-method=token&grant_type=authorization_code&code=${code}&code_verifier=${codeVerifier}`)
       .then(r => r.text());
 
     if (!settings.filesystems[fsUrl]) {
@@ -463,12 +466,57 @@ async function validateUrl(url, settings) {
   return remfsUrl;
 }
 
-function authorize(fsUrl) {
+async function authorize(fsUrl) {
   const clientId = window.location.origin;
   const redirectUri = encodeURIComponent(window.location.href);
   const state = encodeURIComponent(fsUrl);
   const scope = 'type=dir;perm=write;path=/';
-  window.location.href = fsUrl + `?pauth-method=authorize&response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${state}`;
+
+  const pkceCodeVerifier = generateRandomString();
+  localStorage.setItem('pkceCodeVerifier', pkceCodeVerifier);
+
+  const pkceCodeChallenge = await pkceChallengeFromVerifier(pkceCodeVerifier);
+
+  window.location.href = fsUrl
+    + `?pauth-method=authorize`
+    + `&response_type=code`
+    + `&client_id=${clientId}`
+    + `&redirect_uri=${redirectUri}`
+    + `&scope=${scope}`
+    + `&state=${state}`
+    + `&code_challenge=${encodeURIComponent(pkceCodeChallenge)}`
+    + `&code_challenge_method=S256`;
+}
+
+
+// The following functions were taken from:
+// https://github.com/aaronpk/pkce-vanilla-js
+// Generate a secure random string using the browser crypto functions
+function generateRandomString() {
+  const array = new Uint32Array(28);
+  window.crypto.getRandomValues(array);
+  return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('');
+}
+// Calculate the SHA256 hash of the input text. 
+// Returns a promise that resolves to an ArrayBuffer
+function sha256(plain) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return window.crypto.subtle.digest('SHA-256', data);
+}
+// Base64-urlencodes the input string
+function base64urlencode(str) {
+  // Convert the ArrayBuffer to string using Uint8 array to conver to what btoa accepts.
+  // btoa accepts chars only within ascii 0-255 and base64 encodes them.
+  // Then convert the base64 encoded to base64url encoded
+  //   (replace + with -, replace / with _, trim trailing =)
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(str)))
+      .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+// Return the base64-urlencoded sha256 hash for the PKCE challenge
+async function pkceChallengeFromVerifier(v) {
+  const hashed = await sha256(v);
+  return base64urlencode(hashed);
 }
 
 
